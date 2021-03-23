b28dcb1c20031217cf829827e6205705f67f2113
Lajtkek <lajtkep@xdent.cz>
2021-03-23 02:56:40 +0100

    Login

--- FTPClient/Forms/Login.Designer.cs
+++ FTPClient/Forms/Login.Designer.cs
@@ -34,25 +34,25 @@ namespace FTPClient
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.login_btn = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.server_txt = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // username_txt
             // 
-            this.username_txt.Location = new System.Drawing.Point(73, 94);
+            this.username_txt.Location = new System.Drawing.Point(83, 174);
             this.username_txt.Name = "username_txt";
-            this.username_txt.Size = new System.Drawing.Size(181, 20);
+            this.username_txt.Size = new System.Drawing.Size(215, 20);
             this.username_txt.TabIndex = 0;
             this.username_txt.Text = "iktftpucet";
             // 
             // password_txt
             // 
-            this.password_txt.Location = new System.Drawing.Point(73, 120);
+            this.password_txt.Location = new System.Drawing.Point(83, 200);
             this.password_txt.Name = "password_txt";
-            this.password_txt.Size = new System.Drawing.Size(181, 20);
+            this.password_txt.Size = new System.Drawing.Size(215, 20);
             this.password_txt.TabIndex = 1;
             this.password_txt.Text = "Hibiki123";
             this.password_txt.UseSystemPasswordChar = true;
@@ -60,7 +60,7 @@ namespace FTPClient
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 97);
+            this.label1.Location = new System.Drawing.Point(24, 177);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 2;
@@ -69,7 +69,7 @@ namespace FTPClient
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(14, 123);
+            this.label2.Location = new System.Drawing.Point(24, 203);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 3;
@@ -77,7 +77,7 @@ namespace FTPClient
             // 
             // login_btn
             // 
-            this.login_btn.Location = new System.Drawing.Point(179, 173);
+            this.login_btn.Location = new System.Drawing.Point(223, 255);
             this.login_btn.Name = "login_btn";
             this.login_btn.Size = new System.Drawing.Size(75, 23);
             this.login_btn.TabIndex = 4;
@@ -85,50 +85,55 @@ namespace FTPClient
             this.login_btn.UseVisualStyleBackColor = true;
             this.login_btn.Click += new System.EventHandler(this.login_btn_Click);
             // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.server_txt);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.username_txt);
-            this.panel1.Controls.Add(this.login_btn);
-            this.panel1.Controls.Add(this.password_txt);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(280, 199);
-            this.panel1.TabIndex = 5;
-            // 
             // server_txt
             // 
-            this.server_txt.Location = new System.Drawing.Point(73, 149);
+            this.server_txt.Location = new System.Drawing.Point(83, 229);
             this.server_txt.Name = "server_txt";
-            this.server_txt.Size = new System.Drawing.Size(181, 20);
+            this.server_txt.Size = new System.Drawing.Size(215, 20);
             this.server_txt.TabIndex = 7;
             this.server_txt.Text = "slonice.endora.cz/idk.4fan.cz/web/xd";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(29, 152);
+            this.label3.Location = new System.Drawing.Point(39, 232);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(38, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Server";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = global::FTPClient.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(304, 223);
-            this.Controls.Add(this.panel1);
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(323, 292);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.server_txt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.username_txt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.login_btn);
+            this.Controls.Add(this.password_txt);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Login";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.Text = "Desu FTP Client ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -139,9 +144,9 @@ namespace FTPClient
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button login_btn;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TextBox server_txt;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
