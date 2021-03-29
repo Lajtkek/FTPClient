9c47cba89c593b742d59c148d7cf4f4eb9f9e5c2
Lajtkek <lajtkep@xdent.cz>
2021-03-30 01:20:32 +0200

    čeština

--- FTPClient/Forms/Login.Designer.cs
+++ FTPClient/Forms/Login.Designer.cs
@@ -42,17 +42,17 @@ namespace FTPClient
             // 
             // username_txt
             // 
-            this.username_txt.Location = new System.Drawing.Point(83, 174);
+            this.username_txt.Location = new System.Drawing.Point(111, 174);
             this.username_txt.Name = "username_txt";
-            this.username_txt.Size = new System.Drawing.Size(215, 20);
+            this.username_txt.Size = new System.Drawing.Size(187, 20);
             this.username_txt.TabIndex = 0;
             this.username_txt.Text = "iktftpucet";
             // 
             // password_txt
             // 
-            this.password_txt.Location = new System.Drawing.Point(83, 200);
+            this.password_txt.Location = new System.Drawing.Point(111, 200);
             this.password_txt.Name = "password_txt";
-            this.password_txt.Size = new System.Drawing.Size(215, 20);
+            this.password_txt.Size = new System.Drawing.Size(187, 20);
             this.password_txt.TabIndex = 1;
             this.password_txt.Text = "Hibiki123";
             this.password_txt.UseSystemPasswordChar = true;
@@ -60,20 +60,20 @@ namespace FTPClient
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 177);
+            this.label1.Location = new System.Drawing.Point(12, 177);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.Size = new System.Drawing.Size(93, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Username";
+            this.label1.Text = "Uživatelské jméno";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 203);
+            this.label2.Location = new System.Drawing.Point(71, 203);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.Size = new System.Drawing.Size(34, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Password";
+            this.label2.Text = "Heslo";
             // 
             // login_btn
             // 
@@ -87,16 +87,16 @@ namespace FTPClient
             // 
             // server_txt
             // 
-            this.server_txt.Location = new System.Drawing.Point(83, 229);
+            this.server_txt.Location = new System.Drawing.Point(111, 229);
             this.server_txt.Name = "server_txt";
-            this.server_txt.Size = new System.Drawing.Size(215, 20);
+            this.server_txt.Size = new System.Drawing.Size(187, 20);
             this.server_txt.TabIndex = 7;
             this.server_txt.Text = "slonice.endora.cz/idk.4fan.cz/web/xd";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(39, 232);
+            this.label3.Location = new System.Drawing.Point(67, 232);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(38, 13);
             this.label3.TabIndex = 6;
